110b8608284bc6137f258ba9adff7088c078371d
Dennis Kenneth <dennyify123@gmail.com>
2021-05-13 14:57:58 +0000

    1st commit

---
+++ main.py
@@ -0,0 +1,24 @@
+"""
+PIRPLE ASSIGNMENT 2 ON FUNCTIONS
+"""
+
+#FAVOURITE SONG NAME FUNCTIONS
+def songName():
+    name = "What's Next"
+    return name
+
+#FAVOURITE SONG ARTIST FUNCTIONS
+def artist():
+    artistName = "Drake"
+    return artistName
+
+#FAVOURITE SONG RELEASE YEAR FUNCTIONS
+def releaseYear():
+    year = 2021
+    return year
+
+#BOOLEAN TESTING FUNCTION
+def boolean():
+    state = True
+    return state
+
